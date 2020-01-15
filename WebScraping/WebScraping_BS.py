c8a6e2fe1659d1c28bad86eea6deab158ec23004
LordLean <59891036+LordLean@users.noreply.github.com>
2020-01-15 13:59:15 +0000

    Add files via upload

---
+++ WebScraping/WebScraping_BS.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import requests 
+from bs4 import BeautifulSoup 
+import lxml 
+import html 
+
+url = "https://www.myprotein.com/sports-nutrition/impact-whey-protein/10530943.html"
+r = requests.get(url) 
+html = r.text
+soup = BeautifulSoup(html, "lxml")
+
+formatted = []
+matches = soup.find_all("div", class_="productDescription_synopsisContent")[4:5]
+
+for match in matches:
+    formatted += match.text.split("\n")
+    
+for item in formatted:
+    print(item)
+    print(" ")
+
+
+# In[54]:
+
+
+reduced = []
+for item in formatted[2:]:
+    print("")
+    print(item)
+    reduced.append(item) 
+len(reduced)
+
